c5b8e851491097940cba61dddbf8bda2985a3e30
Ayush Dixit <dixitayush5085@gmail.com>
2020-08-30 17:22:02 +0530

    Final

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require("express");
+/*var express = require("express");
 var cors = require('cors');
 var mongoose = require("mongoose");
 const mongodbURL ="mongodb+srv://ayush:ayush123@cluster0.lbv52.mongodb.net/users?retryWrites=true&w=majority"; 
@@ -15,7 +15,7 @@ app.use(cors({
     origin:"*"
 }));
 //body parser 
-app.use(express.urlencoded({extended: false}));
+// app.use(express.urlencoded({extended: false}));
 //Routes
 app.use('/',require('./routes/index')); 
 //user routes
@@ -27,4 +27,20 @@ var PORT = 5000||process.env.PORT||process.env.IP;
 
 app.listen(PORT,()=>{
 	console.log("Server started at 5000");
+});*/
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+
+app.post('/add_news', (req, res) => {
+	res.send('ashav');
+    console.log(req.body);
+});
+
+app.listen(5000, function() {
+    console.log('server is running 5000');
 });
