8a1024b7624b1cd2db3ce8597a249ac63c040ce5
Ayush Dixit <dixitayush5085@gmail.com>
2020-08-30 16:28:15 +0530

    cccc

--- routes/news.js
+++ routes/news.js
@@ -1,13 +1,16 @@
 var express = require("express");
-var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 var router = express.Router();
 
-router.get("/add_news",(req,res)=>res.send("Add News page"));
+router.get("/add_news",urlencodedParser,(req,res)=>res.send("Add News page"));
 
 router.post("/add_news",(req,res)=>{
 	console.log(req.body);
 	res.send(req.body);
 });
-
 
 module.exports = router;
