ee4e7dff7b00f63846df2fc7482d3e9b0f1194e6
satyam <iamsatyam26@gmail.com>
2020-10-09 13:48:07 +0530

    testing

--- app.js
+++ app.js
@@ -48,8 +48,13 @@ app.use('/news',require('./routes/news'));
 
 var PORT = 5000||process.env.PORT||process.env.IP;
 
-app.listen(PORT,()=>{
+/*app.listen(PORT,()=>{
 	console.log("Server started at 5000");
+});
+*/
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 /*const express = require('express');
