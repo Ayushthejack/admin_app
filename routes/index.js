a7a5dbd2ff84282c9bc6f6cb781bc604e4628e1a
Ayush Dixit <dixitayush5085@gmail.com>
2020-10-10 11:25:14 +0530

    tetetet

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ var express = require("express");
 
 var router = express.Router();
 
-router.get("/",(req,res)=>res.send("Welcome"));
+router.get("/",(req,res)=>res.send("Welcome to the Index"));
 
 module.exports = router;
